071adff7a9a1cf615fd76150505763b23fe4fc7d
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-14 18:20:43 -0500

    Added a single test

--- tests/testthat/test-fnsComparison.R
+++ tests/testthat/test-fnsComparison.R
@@ -1,11 +1,41 @@
 
 library(testthat)
 
-data("results_2010", "results_2011")
+old_df = data.frame(var1 = c("A", "B", "C"),
+                    val1 = c(1, 2, 3))
+
+new_df = data.frame(var1 = c("A", "B", "C"),
+                    val1 = c(1, 2, 4))
+
+context("compare_df_function")
+#===============================================================================
+# basic tests
+ctable = compare_df(new_df, old_df, c("var1"))
+
+expected_comparison_df = data.frame(var1 = ("C"), chng_type = c("+", "-"), val1 = c(4,3))
+expect_equal(expected_comparison_df, ctable$comparison_df)
 
 #===============================================================================
-context("Basic Comparison")
+# Test for sameness
 
-ctable = compare_df(results_2011, results_2010, c("Student"), exclude = "Discipline")
+# Error if chng_type is used
 
-ctable$html_output
+# Test for sameness after exclusion
+
+# Table
+
+# Multiple Gourping
+
+# Exclude
+
+# Limit
+
+
+#===============================================================================
+contect("Exclude")
+
+#===============================================================================
+context("limit")
+
+#limit warning
+
