dbaeaa20a08b9520ae2ef905373d48dab973334b
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-23 18:18:46 -0500

    Updated detauls about the data frames

--- R/zzz.R
+++ R/zzz.R
@@ -2,17 +2,28 @@
   options(stringsAsFactors = FALSE) # nocov
 }
 
-#' Cerated data set to show off the package capabilities - old_df
+#' Data set created set to show off the package capabilities - Results of students for 2010
 #'
 #' A manually created dataset showing the hypothetical scores of two divisions of students
-#'
-#' @format A data frame some rows and some columns
+#' \itemize{
+#'   \item Division The division to which the student belongs
+#'   \item Student Name of the Student
+#'   \item Maths, Physics, Chemistry, Art Scores of the student across different subjects
+#'   \item Discipline, PE Grades of the students across different subjects
+#' }
+#' @format A data frame 12 rows and 8 columns
 "results_2010"
 
 
-#' Cerated data set to show off the package capabilities - new_df
+#' Data set created set to show off the package capabilities - Results of students for 2011
 #'
 #' A manually created dataset showing the hypothetical scores of two divisions of students
+#' \itemize{
+#'   \item Division The division to which the student belongs
+#'   \item Student Name of the Student
+#'   \item Maths, Physics, Chemistry, Art Scores of the student across different subjects
+#'   \item Discipline, PE Grades of the students across different subjects
+#' }
 #'
-#' @format A data frame some rows and some columns
+#' @format A data frame 13 rows and 8 columns
 "results_2011"
